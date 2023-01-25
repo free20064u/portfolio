b97d464707ce5671fccd19283d6a4412255ea0b1
Alfred <76535763+free20064u@users.noreply.github.com>
2023-01-25 13:33:24 +0000

    navbar added pc and mobile

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
-import logo from '../../asserts/images/logo.png'
+import './navbar.css';
+import logo from '../../asserts/images/logo.png';
+import menu from '../../asserts/icons/menu.png';
+import close from '../../asserts/icons/close.png'
 
 function Navbar() {
+
+  const links = [
+    {
+      id: 1,
+      path: 'Home'
+    },
+    {
+      id: 2,
+      path: 'Services'
+    },
+    {
+      id: 3,
+      path: 'Contact'
+    }
+  ]
   return (
     <div className='navbar container'>
-      <div><h1>Alfswebdev</h1></div>
+      <div id='nameLogo'>
+        <img src={logo} id="logo" alt="logo" />
+        <div><h1>Alfswebdev</h1></div>
+      </div>
       <div>
-        <nav>
-          <ul>
-            <li>Home</li>
-            <li>Services</li>
-            <li>Contact</li>
+        <img src={menu} id="menu" alt="menu" />
+        <img src={close} id="close" alt="close" />
+        <nav className='nav'>
+          <ul className='nav-links'>
+            {links.map(link => <li className='nav-link'><a href="#" className='link'>{link.path}</a></li>)}
           </ul>
         </nav>
       </div>
