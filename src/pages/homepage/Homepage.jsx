d2bc596ae5b3dfa3d1e1b3199f9697967d00b8f0
Alfred <76535763+free20064u@users.noreply.github.com>
2023-01-25 23:35:41 +0000

    homepage pc and mobile done

---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './homepage.css';
+
+function Homepage() {
+  return (
+    <div className="container intro">
+      <h1 id='intro-remark'>WELCOME TO ALFSWEBDEV</h1>
+      <h2 id='slogan'>home of modern website</h2>
+      <button className='intro-btn'>Check my work</button>
+    </div>
+  )
+}
+
+export default Homepage
