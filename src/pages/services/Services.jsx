361ed0420c42f2a8df0f7fcd5d4f7df0095b039a
Alfred <76535763+free20064u@users.noreply.github.com>
2023-02-10 01:42:57 +0000

    skill and tool changes to cards

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -2,32 +2,102 @@ import React from 'react';
 import './services.css';
 import Swiper from '../../components/swipper/Swiper';
 import Card from '../../components/cards/Card';
+import django from '../../asserts/svg/django.svg';
 
 function Services() {
+  const skills = [
+    {
+      id: 1,
+      name: 'Django',
+      image: django,
+      content: 'We use django for handling of fullstack website that is for both the frontend and the backend. We also use it for developing API for our cliend.'
+    },
+    {
+      id: 2,
+      name: 'MySql',
+      image: django,
+      content: 'Mysql is the data handler for the backend. Mysql is very effective in handling huge database and also very secured'
+    },
+    {
+      id: 3,
+      name: 'Sqlite',
+      image: django,
+      content: 'Sqlite is used for handling of small database used by a single client of the project being worked on'
+    },
+    {
+      id: 4,
+      name: 'React js',
+      image: django,
+      content: 'This is used for the creation of the frontend of the website more specifically single page applications'
+    }
+  ]
+
+  const tools = [
+    {
+      id: 1,
+      name: "Adobe Ai",
+      image: django,
+      description: "Adobe Ai has some excellent features that enable us to design beautiful mordern icons for our clients."
+    },
+    {
+      id: 2,
+      name: "Adobe Xd",
+      image: django,
+      description: "Adobe Xd have some excellent features that helps to design the User Interface and User experience of the for our client even before we begin to develop it."
+    },
+    {
+      id: 3,
+      name: "Github",
+      image: django,
+      description: "Its an excellent tool that enable us to safely store our project online. It also gives the flexibility of collaboration among project developers"
+    },
+    {
+      id: 4,
+      name: "VScode",
+      image: django,
+      description: "A very simple but yet majestic tool that is used for wrighting of the code for the website."
+    }
+  ]
   return (
     <div className='container'>
-      <div id='skills-tools'>
-        <div>
-          <h2>Skills</h2>
-          <ul>
-            <li>Css</li>
-            <li>Django</li>
-            <li>Html</li>
-            <li>Javascript</li>
-            <li>Python</li>
-            <li>React Js</li>
-          </ul>
+      <div className="skills">
+        <h2>Skills</h2>
+        <div className='service-cards'>
+        {skills.map(tool => 
+        <div className='service-card'>
+          <h4 className='service-card-title'>{tool.name}</h4>
+          <img className='service-image' src={tool.image} alt={tool.name}/>
+          <p className='service-para'>{tool.content}</p>
         </div>
-        <div>
-        <h2>Tools</h2>
-          <ul>
-            <li>Adobe Ai</li>
-            <li>Adobe Xd</li>
-            <li>Github</li>
-            <li>Vscode</li>
-          </ul>
+        )}
         </div>
       </div>
+      <hr />
+
+      <div className="tools">
+        <h2>Tools</h2>
+        <div className='service-cards'>
+        {tools.map(skill => 
+        <div className='service-card'>
+          <h4 className='service-card-title'>{skill.name}</h4>
+          <img className='service-image' src={skill.image} alt={skill.name} />
+          <p className='service-para'>{skill.description}</p>
+        </div>
+        )}
+        </div>
+      </div>
+      <hr />
+      
+      <div>
+        <h2>Languages</h2>
+        <ul>
+          <li>Css</li>
+          <li>Html</li>
+          <li>Javascript</li>
+          <li>Python</li>
+        </ul>
+      </div>
+      
       <div id='projects'>
         <h2 id='project-title'>Projects</h2>
         <div>
